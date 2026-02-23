d0b1dc6fc49a764894a588f54565f3bec5a874b1
Pavel Zbytovský <zbytovsky@gmail.com>
2026-02-23 19:39:10 +0100

    maplibre

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div style={{ position: 'absolute', top: 0, left: 0, padding: 10, zIndex: 1 }}>
+      <h1>MapLibre GL JS Example</h1>
+    </div>
   </React.StrictMode>,
 );
+
+
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://demotiles.maplibre.org/style.json',
+    center: [-74.5, 40], // starting position
+    zoom: 2, // starting zoom
+    rollEnabled: true // Enable mouse control of camera roll angle with `Ctrl` + right-click and drag
+});
+
+
+// Add zoom and rotation controls to the map.
+map.addControl(new maplibregl.NavigationControl({
+    visualizePitch: true,
+    visualizeRoll: true,
+    showZoom: true,
+    showCompass: true
+}));
