54714a28682d6ca9f538641c0048c16eb4f07587
Pavel Zbytovský <zbytovsky@gmail.com>
2026-02-25 15:44:38 +0100

    alles gutte

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // @ts-expect-error process is a nodejs global
-const host = '0.0.0.0' ?? process.env.TAURI_DEV_HOST;
+const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
